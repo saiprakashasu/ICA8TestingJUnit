d7f5a0a866b3fc57b1093481f713805428d2109c
saiprakashasu <sravic24@asu.edu>
2022-10-27 22:43:12 -0700

    keyboard input implemented

--- src/main/java/org/example/urinals.java
+++ src/main/java/org/example/urinals.java
@@ -6,7 +6,7 @@ public class urinals {
     //static String urinalString;
     static int l;
 
-    public static String getUrinalString() {
+    public static String getUrinalStringKeyboard() {
         //System.out.println("Not yet implemented");
         String urinalString;
         Scanner sc1 = new Scanner(System.in);
@@ -18,7 +18,35 @@ public class urinals {
     public static int countUrinals(String urinalString){
 
         int count = 0;
+        count = checkValidString(urinalString);
+        if(count!=-1){
+            String x = String.valueOf(urinalString.charAt(0));
+            String y = String.valueOf(urinalString.charAt(1));
+            String z;
+            StringBuilder sb = new StringBuilder(urinalString);
 
+            if(x.equals("0") & y.equals("0")){
+                sb.setCharAt(0, '1');
+                count++;
+            }
+
+            for (int i = 2; i < l - 2; i++) {
+                x = String.valueOf(sb.charAt(i-1));
+                y = String.valueOf(sb.charAt(i));
+                z = String.valueOf(sb.charAt(i+1));
+                if (x.equals("0") & y.equals("0") & z.equals("0")) {
+                    sb.setCharAt(i, '1');
+                    count++;
+                }
+            }
+
+            x = String.valueOf(sb.charAt(l-2));
+            y = String.valueOf(sb.charAt(l-1));
+            if(x.equals("0") & y.equals("0")){
+                sb.setCharAt(l-1, '1');
+                count++;
+            }
+        }
 
         return count;
     }
@@ -44,8 +72,18 @@ public class urinals {
     }
 
     public static void main(String[] args) {
-        String urinalString = getUrinalString();
-        //int ln = urinalString.length();
-        countUrinals(urinalString);
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Press 1. for keyboard input 2. for file input");
+        String in = sc1.next();
+        if(in.equals("1")){
+            while(true) {
+                String urinalString = getUrinalStringKeyboard();
+                if(urinalString.equals("0")){break;}
+                int res = countUrinals(urinalString);
+                System.out.println(res);
+            }
+        }
+
     }
 }
--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -12,21 +12,86 @@ class urinalsTest {
 
     @Test
     @DisplayName("count Urinals")
-    void countUrinals() {
+    void countUrinals1() {
         urinals u = new urinals();
         assertEquals(0, u.countUrinals("1001"));
         System.out.println("Sai Prakash Test one executed");
     }
     @Test
-    @DisplayName("valid String")
-    void checkValidString() {
+    @DisplayName("count Urinals")
+    void countUrinals2() {
+        urinals u = new urinals();
+        assertEquals(1, u.countUrinals("10001"));
+        System.out.println("Sai Prakash Test two executed");
+    }
+    @Test
+    @DisplayName("count Urinals")
+    void countUrinals3() {
+        urinals u = new urinals();
+        assertEquals(3, u.countUrinals("00000"));
+        System.out.println("Sai Prakash Test three executed");
+    }
+    @Test
+    @DisplayName("count Urinals")
+    void countUrinals4() {
+        urinals u = new urinals();
+        assertEquals(-1, u.countUrinals("011"));
+        System.out.println("Sai Prakash Test four executed");
+    }
+    @Test
+    @DisplayName("count Urinals")
+    void countUrinals5() {
+        urinals u = new urinals();
+        assertEquals(1, u.countUrinals("01000"));
+        System.out.println("Sai Prakash Test five executed");
+    }
+
+    @Test
+    @DisplayName("count Urinals")
+    void countUrinals6() {
+        urinals u = new urinals();
+        assertEquals(2,u.countUrinals("0000"));
+        System.out.println("Sai Prakash Test six executed");
+    }
+    @Test
+    @DisplayName("valid String 1")
+    void checkValidString1() {
         urinals u = new urinals();
         assertEquals(-1, u.checkValidString("0011"));
         assertEquals(0, u.checkValidString("101010"));
         assertEquals(0,u.checkValidString("00000"));
         assertEquals(-1, u.checkValidString("101010010101010101010101010101010"));
         assertEquals(-1, u.checkValidString(""));
-        System.out.println("Sai Prakash Test two executed");
+        System.out.println("Sai Prakash Test seven executed");
     }
+    @Test
+    @DisplayName("valid String 2")
+    void checkValidString2() {
+        urinals u = new urinals();
+        assertEquals(0, u.checkValidString("101010"));
+        System.out.println("Sai Prakash Test eight executed");
+    }
+    @Test
+    @DisplayName("valid String 3")
+    void checkValidString3() {
+        urinals u = new urinals();
+        assertEquals(0,u.checkValidString("00000"));
+        System.out.println("Sai Prakash Test nine executed");
+    }
+    @Test
+    @DisplayName("valid String 4")
+    void checkValidString4() {
+        urinals u = new urinals();
+        assertEquals(-1, u.checkValidString("101010010101010101010101010101010"));
+        System.out.println("Sai Prakash Test ten executed");
+    }
+    @Test
+    @DisplayName("valid String 5")
+    void checkValidString5() {
+        urinals u = new urinals();
+        assertEquals(-1, u.checkValidString(""));
+        System.out.println("Sai Prakash Test eleven executed");
+    }
+
 
 }
