d4058b886cccf961358e47b09461c26ca40700db
saiprakashasu <sravic24@asu.edu>
2022-10-27 22:13:01 -0700

    checkValidString implemented

--- src/main/java/org/example/urinals.java
+++ src/main/java/org/example/urinals.java
@@ -3,23 +3,49 @@ package org.example;
 import java.util.Scanner;
 
 public class urinals {
-    static String urinalString;
+    //static String urinalString;
+    static int l;
 
-    public static void main(String[] args) {
-        getUrinalString();
-    }
-    public static void getUrinalString() {
+    public static String getUrinalString() {
         //System.out.println("Not yet implemented");
+        String urinalString;
         Scanner sc1 = new Scanner(System.in);
         System.out.println("Enter your urinals string");
         urinalString = sc1.next();
+        return urinalString;
     }
 
-    public static int countUrinals(){
-        return 0;
+    public static int countUrinals(String urinalString){
+
+        int count = 0;
+
+
+        return count;
     }
 
-    public static int checkValidString(){
-        return 0;
+    public static int checkValidString(String urinalString){
+        l = urinalString.length();
+        int c=0;
+        if(l>20 || l<1){
+            c=-1;
+        }
+        else {
+            String x, y;
+            for (int i = 0; i < l - 1; i++) {
+                x = String.valueOf(urinalString.charAt(i));
+                y = String.valueOf(urinalString.charAt(i + 1));
+                if (x.equals("1") & y.equals("1")) {
+                    c = -1;
+                    break;
+                }
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        String urinalString = getUrinalString();
+        //int ln = urinalString.length();
+        countUrinals(urinalString);
     }
 }
--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -14,7 +14,19 @@ class urinalsTest {
     @DisplayName("count Urinals")
     void countUrinals() {
         urinals u = new urinals();
-        assertEquals(0, u.countUrinals());
+        assertEquals(0, u.countUrinals("1001"));
         System.out.println("Sai Prakash Test one executed");
     }
+    @Test
+    @DisplayName("valid String")
+    void checkValidString() {
+        urinals u = new urinals();
+        assertEquals(-1, u.checkValidString("0011"));
+        assertEquals(0, u.checkValidString("101010"));
+        assertEquals(0,u.checkValidString("00000"));
+        assertEquals(-1, u.checkValidString("101010010101010101010101010101010"));
+        assertEquals(-1, u.checkValidString(""));
+        System.out.println("Sai Prakash Test two executed");
+    }
+
 }
